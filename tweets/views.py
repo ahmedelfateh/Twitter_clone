c8cb604c4b6411fe422d715cb35a3a0fe3864f8e
AhmedElfateh <elfateh.dev@gmail.com>
2019-01-13 14:16:38 +0200

    form validation with user login

--- tweets/views.py
+++ tweets/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy, reverse
 
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView

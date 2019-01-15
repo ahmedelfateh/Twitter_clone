c67cb6c3811c253d8aebe8b3cfcee0c285947495
Ahmed Elfateh <elfateh.dev@gmail.com>
2019-01-15 14:27:44 +0200

    add letter counter to the tweet submit view (#3)

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name='list'),  # /api/tweet/
-    url(r'^create$', TweetCreateAPIView.as_view(),
+    url(r'^create/$', TweetCreateAPIView.as_view(),
         name='create'),  # api/tweet/create
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), #/tweet/detail/
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/update/

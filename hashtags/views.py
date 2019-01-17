2b86c455620191551785cbc3cde0a9d4f14591ac
AhmedElfateh <elfateh.dev@gmail.com>
2019-01-17 20:16:20 +0200

    create hashtags model / create hashtags view func / create hashtags template view / fix typo issue in tweets/views.py

--- hashtags/views.py
+++ hashtags/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
+
+from .models import HashTag
 
 # Create your views here.
+
+
+class HashTagView(View):
+    def get(self, request, hashtag, *args, **kwargs):
+        obj, created = HashTag.objects.get_or_create(tag=hashtag)
+        return render(request, 'hashtags/tag_view.html', {"obj": obj})
